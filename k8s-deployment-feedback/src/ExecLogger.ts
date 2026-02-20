3c2a76755a750595d8ad63d54e2db3a497700878
Nicklas Utgaard <nutgaard@gmail.com>
2026-02-20 08:22:36 +0100

    noJira: fikser opplasting av replay artifakt

--- k8s-deployment-feedback/src/ExecLogger.ts
+++ k8s-deployment-feedback/src/ExecLogger.ts
@@ -2,7 +2,7 @@ import * as fs from "node:fs";
 import * as core from '@actions/core';
 
 export class ExecLogger {
-    private readonly outputDir = ".capture";
+    private readonly outputDir = process.env.CAPTURE_DIR ?? "capture";
     private readonly entries: Map<string, number> = new Map();
 
     append(args: string[], result: string) {
