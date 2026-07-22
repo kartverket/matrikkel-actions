9a553d6c57a661c7c78b6452fd43117d944d81b1
Nicklas Utgaard <nutgaard@gmail.com>
2026-07-22 09:37:44 +0200

    nojira: utvide DB-metadata-schema med muligheten for å gi flere navn
    gjøres bakoverkompatibelt ved å introdusere "names" som nytt felt.

--- skip-deploy/src/expansions/expansion-rules/rule.testutils.ts
+++ skip-deploy/src/expansions/expansion-rules/rule.testutils.ts
@@ -3,7 +3,8 @@ import * as yaml from 'yaml';
 import {
     ApplicationExpansionContext,
 } from "../ApplicationExpansionContext.ts";
-import type {DatabaseMetadata, DatabaseMetadataResolver} from "./databasesRule.ts";
+import {DatabaseMetadata, DatabaseMetadataFile, getNamesFromDBMetadata} from "./databasesRule.ts";
+import type { DatabaseMetadataResolver} from "./databasesRule.ts";
 
 
 export function testContext(manifest: string, dbMetadata: string | null = null): ApplicationExpansionContext {
@@ -18,9 +19,15 @@ export function testContext(manifest: string, dbMetadata: string | null = null):
 
 export function testDbResolver(data: string | null): DatabaseMetadataResolver {
     const parsed = data === null ? null : yaml.parse(data);
+    const metadata = DatabaseMetadataFile.safeParse(parsed);
     return async (namespace, databaseName) => {
         if (parsed === null) throw new Error('No Db configured');
-        return parsed.databases.find((it: any) => it.name === databaseName) as DatabaseMetadata;
+
+        const databasemetadata: DatabaseMetadata | undefined = metadata.data?.databases
+            .find(it => getNamesFromDBMetadata(it).includes(databaseName))
+        if (databasemetadata == null) throw new Error(`No Db configured named ${databaseName}`);
+
+        return databasemetadata;
     }
 }
 
